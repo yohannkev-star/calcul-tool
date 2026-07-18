be23dc8bdfe8e71bed72f8cfb20fe2180b9415eb
yohannkev-star <yohannkev@gmail.com>
2026-07-18 12:07:14 +0200

    feat(calcul): ajout de la fonction multiplication

--- calcul.py
+++ calcul.py
@@ -1,14 +1,17 @@
 # calcul.py
 def addition(a, b):
-    # BUG: avant on retournait a - b par erreur
-    # CORRECTION: on retourne maintenant a + b
     return a + b
 
 def soustraction(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
+
 if __name__ == "__main__":
     result_add = addition(5, 3)
     result_sub = soustraction(5, 3)
+    result_mul = multiplication(4, 2)
     print(f"Addition : {result_add}")
     print(f"Soustraction : {result_sub}")
+    print(f"Multiplication : {result_mul}")
