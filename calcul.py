2ccc61e94e17e8386a0695cdb656a0f14170ea39
yohannkev-star <yohannkev@gmail.com>
2026-07-18 12:23:19 +0200

    chore(main): mise a jour commentaire sur main

--- calcul.py
+++ calcul.py
@@ -1,7 +1,12 @@
-# calcul.py
+# calcul.py - Version main
 def addition(a, b):
     return a + b
 
+def soustraction(a, b):
+    return a - b
+
 if __name__ == "__main__":
-    result = addition(5, 3)
-    print(f"Résultat : {result}")
+    result_add = addition(5, 3)
+    result_sub = soustraction(5, 3)
+    print(f"Addition : {result_add}")
+    print(f"Soustraction : {result_sub}")
